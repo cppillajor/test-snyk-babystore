82663d481ea7367e3861213bb92b41cc157482db
cppillajor <cristianpillajo1999@gmail.com>
2024-01-29 17:15:50 -0500

    cuarto

--- src/servers/server.ts
+++ src/servers/server.ts
@@ -12,14 +12,14 @@ const app: express.Application = express();
 
 const initMiddlewares = () => {
   //app.use(morgan("dev", { stream }));
-  app.use(cors({ origin: true, credentials: true }));
-  app.use(hpp());
-  app.use(helmet(helmetConf));
-  app.use(compression());
+  //app.use(cors({ origin: true, credentials: true }));
+  //app.use(hpp());
+  //app.use(helmet(helmetConf));
+  //app.use(compression());
   app.use(multer().any());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  //app.use("/api-docs", serve, setup(specificationsSwagger));
+  ///app.use("/api-docs", serve, setup(specificationsSwagger));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 };
@@ -41,7 +41,6 @@ const AppServer = () => {
   routes();
   listen();
 };
-
 
 
 
